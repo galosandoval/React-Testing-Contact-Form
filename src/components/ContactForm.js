29716bcda7dde80fe341a0bc47b7b85db17bde24
Galo Sandoval <galosan92@gmail.com>
2020-08-06 15:33:21 -0700

    finished mvp

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -38,24 +38,28 @@ const ContactForm = () => {
         </div>
 
         <div>
-          <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
-            Email*
-          </label>
-          <input name="email" ref={register({ required: true })} />
+          <label htmlFor="email">Email*</label>
+          <input
+            name="email"
+            ref={register({ required: true })}
+            placeholder="bluebill1049@hotmail.com"
+          />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" ref={register({ required: false })} />
+          <textarea id="message" name="message" ref={register({ required: true })} />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
             {JSON.stringify(data, null, 2)}
           </pre>
         )}
-        <input type="submit" />
+        <input 
+        id="submit"
+        type="submit" />
       </form>
     </div>
   );
--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -5,20 +5,37 @@ import {
   fireEvent,
   getByLabelText,
   findByLabelText,
+  getByTestId,
+  findByTestId,
 } from "@testing-library/react";
 import ContactForm from "./ContactForm";
-import "@testing-library/jest-dom/extend-expect";
+import { change } from "@testing-library/jest-dom/extend-expect";
 
 test("renders App without crashing", () => {
   render(<ContactForm />);
 });
-// find the inputs
-const firstName = screen.getByLabelText(/first name/i);
-const lastName = screen.findByLabelText(/last name*/i);
-const email = screen.findByLabelText(/email*/i);
-const message = screen.findByLabelText(/message/i);
-// add text to inputs
-fireEvent.change(firstName, { target: { value: "g" } });
-// find submit button
-// click the submit button
-// make sure the submitted data renders to the DOM
+
+test("can input and submit info", async () => {
+  render(<ContactForm />);
+
+  // find the inputs
+  const firstName = screen.getByPlaceholderText(/edd/i)
+  const lastName = screen.getByPlaceholderText(/burke/i);
+  const email = screen.getByPlaceholderText(/bluebill1049@hotmail.com/i);
+  const message = await screen.findByLabelText(/message/i);
+  
+  // add text to inputs
+  fireEvent.change(firstName, { target: { value: "g" } });
+  fireEvent.change(lastName, { target: { value: "g" } });
+  fireEvent.change(email, { target: { value: "g@g.com" } });
+  fireEvent.change(message, { target: { value: "teehee lol" } });
+  
+  // find submit button
+  const submitButton = screen.getByRole('button', { name: 'Submit' })
+
+  // click the submit button
+  fireEvent.click(submitButton)
+
+  // make sure the submitted data renders to the DOM
+  expect(screen.getByText(/g/i)).toBeInTheDocument();
+});
